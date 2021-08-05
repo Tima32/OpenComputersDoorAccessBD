890375b95e1ac0500430e4b3f44d31a00a886e08
Tima32 <55636583+Tima32@users.noreply.github.com>
2021-08-06 01:11:57 +0300

    Add files via upload

--- DoorAccessBD.lua
+++ DoorAccessBD.lua
@@ -13,13 +13,21 @@ local event = require("event")
 --global
 local clien_table = {}
 
+function SaveClientTable()
+    local table = ser.serialize(client_table);
+    local file = io.open(client_table_adr, "w");
+    file:write(table);
+    file:close();
+end
 function LoadClientTable()
     local file =  io.open(client_table_adr, "r")
     if file ~= nil then
         local table = file:read();
         client_table = ser.unserialize(table);
+        file:close();
     end
 
+    --Если админа нет добавим
     if client_table[default_admin] == nil then
         client_table[default_admin]["password"] = default_admin_pass;
         local access = {};
@@ -27,11 +35,13 @@ function LoadClientTable()
         access["write"] = true;
         access["configure"] = true;
         client_table[default_admin]["access"] = access;
+
+        SaveClientTable();
     end
 end
 
 function main()
-
+    LoadClientTable();
 end
 
 
