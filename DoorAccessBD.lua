03280696ab48b259f052895febf590bca39cf48d
Tima32 <55636583+Tima32@users.noreply.github.com>
2021-08-06 00:23:29 +0300

    Add files via upload

---
+++ DoorAccessBD.lua
@@ -0,0 +1,8 @@
+local ser = require("serialization")
+local com = require("component")
+local event = require("event")
+
+--propirties
+local port = 32001;
+local client_properties_f = "client_properties.json";
+local bd_f = "DoorAccessBD.json"
