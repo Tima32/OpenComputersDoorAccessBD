a3b4afc600228006bcecd4138f994718cda699a1
Tima32 <55636583+Tima32@users.noreply.github.com>
2021-08-06 01:01:53 +0300

    Add files via upload

--- DoorAccessBD.lua
+++ DoorAccessBD.lua
@@ -1,8 +1,38 @@
+--propirties
+local port = 32001;
+local client_table_adr = "client_table.json";
+local bd_f = "DoorAccessBD.json"
+local default_admin = "admin"     
+local default_admin_pass = "admin" --edit in $client_table_adr file
+
+--imporst
 local ser = require("serialization")
 local com = require("component")
 local event = require("event")
 
---propirties
-local port = 32001;
-local client_properties_f = "client_properties.json";
-local bd_f = "DoorAccessBD.json"
+--global
+local clien_table = {}
+
+function LoadClientTable()
+    local file =  io.open(client_table_adr, "r")
+    if file ~= nil then
+        local table = file:read();
+        client_table = ser.unserialize(table);
+    end
+
+    if client_table[default_admin] == nil then
+        client_table[default_admin]["password"] = default_admin_pass;
+        local access = {};
+        access["read"] = true;
+        access["write"] = true;
+        access["configure"] = true;
+        client_table[default_admin]["access"] = access;
+    end
+end
+
+function main()
+
+end
+
+
+main()
