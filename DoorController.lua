8f2942b91994aa27f0ed3717197d92bc7ba8805c
Tima32 <55636583+Tima32@users.noreply.github.com>
2021-08-06 06:56:29 +0300

    Add files via upload

--- DoorController.lua
+++ DoorController.lua
@@ -1,6 +1,6 @@
 --properties
 local door_access_BD_port = 32001;
-local door_access_BD_adr = ""
+local door_access_BD_adr = "97bb736b"
 local port = 32002;
 local doors_adr = "doors_table.json"
 local controllers_adr = "controllers_table.json"
@@ -22,6 +22,23 @@ function EventHandling(event)
 
     if event[1] == "interrupted" then
         os.exit();
+    end
+
+    if event[1] == "modem_message" then
+
+    end
+end
+function EventModemMessage(event)
+    --нужно дополнить адрес?
+    if string.len(door_access_BD_adr) ~= 32 and string.match(event[3], door_access_BD_adr) == 1 then
+        local file = io.open("DoorController.lua", "r")
+        local code = "";
+        file:read(code);
+        file:close()
+        string.gsub(code, door_access_BD_adr, event[3]);
+        file = io.open("DoorController.lua", "w")
+        file:write(code);
+        file:close();
     end
 end
 
