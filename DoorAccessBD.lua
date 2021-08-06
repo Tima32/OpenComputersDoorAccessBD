4949f69645b6c549c2f88c68d9374cd73e324624
Tima32 <55636583+Tima32@users.noreply.github.com>
2021-08-06 04:52:26 +0300

    Add files via upload

--- DoorAccessBD.lua
+++ DoorAccessBD.lua
@@ -1,6 +1,7 @@
 --propirties
 local port = 32001;
 local client_table_adr = "client_table.json";
+local door_controller_table_adr = "door_controller_table.json"
 local bd_adr = "DoorAccessBD.json"
 local default_admin = "admin"     
 local default_admin_pass = "admin" --edit in $client_table_adr file
@@ -12,6 +13,7 @@ local event = require("event")
 
 --global
 local client_table = {}
+local door_controller_table = {}
 local bd = {}
 local modem = com.modem;
 
@@ -44,9 +46,46 @@ function SaveClientTable()
     file:close();
 end
 
+--Door controller table
+function LoadDoorTable()
+    local file =  io.open(door_controller_table_adr, "r")
+
+    if file ~= nil then
+        local table = file:read();
+        door_controller_table = ser.unserialize(table);
+        file:close();
+    end
+end
+function SaveDoor then
+    local table = ser.serialize(door_controller_table);
+    local file = io.open(door_controller_table_adr, "w");
+    file:write(table);
+    file:close();
+end
+
 --BD
 function LoadBD()
     local file =  io.open(bd_adr, "r")
+
+    --пример
+    if file == nil then
+        --bd["door_id"]["players"] = {"Tim", "Admin"}
+        --bd["door_id"]["cards"] = {"id1", "id2"}
+        --bd["door_id"]["password"] = "123"
+
+        local players = {"Admin"}
+        local cards = {"1d5c8a18-17e9-4d2e-8548-f7ebc5cd5f35"}
+        local password = "123"
+
+        local door = {}
+        door["players"] = players;
+        door["cards"] = cards;
+        door["password"] = password;
+
+        bd["door_id"] = door;
+        SaveBD();
+    end
+
     if file ~= nil then
         local table = file:read();
         bd = ser.unserialize(table);
